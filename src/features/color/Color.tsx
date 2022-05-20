729fe692de101e054a85fca80a936b85957b8da5
Maxim <74974283+maximka76667@users.noreply.github.com>
2022-05-20 09:25:18 +0200

    Color styles

--- src/features/color/Color.tsx
+++ src/features/color/Color.tsx
@@ -1,9 +1,10 @@
 import React, { ChangeEventHandler, FormEventHandler, useState } from 'react'
 import { useAppDispatch, useAppSelector } from '../../app/hooks'
 import { change, selectColors } from './colorSlice'
+import './Color.sass'
 
 const Color = () => {
-  const { color, darker, brighter, mix } = useAppSelector(selectColors);
+  const { color, darker, brighter } = useAppSelector(selectColors);
   const dispatch = useAppDispatch();
 
   const [colorValue, setColorValue] = useState("");
@@ -18,14 +19,28 @@ const Color = () => {
   }
 
   return (
-    <div>
+    <div className='color'>
       <form onSubmit={handleSubmit}>
         <input type="text" name="color" onChange={handleChange} />
         <button type="submit">Change color</button>
-        <p style={{ backgroundColor: darker, color: brighter }}>{darker}</p>
-        <p style={{ backgroundColor: color, color: mix }}>{color}</p>
-        <p style={{ backgroundColor: brighter, color: darker }}>{brighter}</p>
       </form>
+      <div className='color__list'>
+        <div className='color__card' style={{ backgroundColor: darker }}>
+          <div className='color__info'>
+            <p className="color__value">{darker}</p>
+          </div>
+        </div>
+        <div className='color__card' style={{ backgroundColor: color }}>
+          <div className='color__info'>
+            <p className="color__value">{color}</p>
+          </div>
+        </div>
+        <div className='color__card' style={{ backgroundColor: brighter }}>
+          <div className='color__info'>
+            <p className="color__value">{brighter}</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
